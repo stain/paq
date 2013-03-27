c4f3e67edb42f382edcd002fab01fdab0862a414
Stian Soiland-Reyes <stian@soiland-reyes.com>
2013-03-27 16:29:13 +0000

    tab->space

--- src/main/java/com/example/provaq/rest/HelloWorld.java
+++ src/main/java/com/example/provaq/rest/HelloWorld.java
@@ -22,48 +22,48 @@ public class HelloWorld {
 
     private static final String HAS_PROVENANCE = "http://www.w3.org/ns/prov#has_provenance";
 
-	@GET
+    @GET
     @Path("hello/{name}")
     @Produces("text/plain")
     public Response hello(@PathParam("name") String name, @Context UriInfo ui) {
-    	// TODO: Could have used Link.fromResourceMethod instead, 
-		// but it seems to return wrong URI in current CXF
-    	URI provUri = ui.getBaseUriBuilder().path(getClass(), "helloProvenance").build(name);
-    	Link provLink = Link.fromUri(provUri).rel(HAS_PROVENANCE).build();
+        // TODO: Could have used Link.fromResourceMethod instead, 
+        // but it seems to return wrong URI in current CXF
+        URI provUri = ui.getBaseUriBuilder().path(getClass(), "helloProvenance").build(name);
+        Link provLink = Link.fromUri(provUri).rel(HAS_PROVENANCE).build();
 
-    	String greeting = "Hello, " + name + "\n";
-    	ResponseBuilder responseBuilder = Response.ok().entity(greeting);
-		return responseBuilder.header(HttpHeaders.LINK, provLink).build();
+        String greeting = "Hello, " + name + "\n";
+        ResponseBuilder responseBuilder = Response.ok().entity(greeting);
+        return responseBuilder.header(HttpHeaders.LINK, provLink).build();
     }
-	
+    
     @GET
     @Path("provenance/hello/{name}")
     @Produces("text/provenance-notation")
     public String helloProvenance(@PathParam("name") String name,
-    		@Context UriInfo ui) throws IOException {
-    	// Get our absolute URI
-    	// See http://cxf.apache.org/docs/jax-rs-basics.html#JAX-RSBasics-URIcalculationusingUriInfoandUriBuilder    	
-    	UriBuilder appUri = ui.getBaseUriBuilder();
-    	// Absolute URIs for resources we are to give provenance about
-    	URI helloURI = appUri.path(getClass(), "hello").build(name);    	
-    	
-    	// Prepare prefixes for PROV-N qualified names
-    	URI appURI = appUri.build("").resolve("../");    	
-    	URI helloPrefix = helloURI.resolve("./");
-    	
-    	// The PROV-N qualified name for our /hello/{name} resource
-    	String helloEntity = "hello:" + helloPrefix.relativize(helloURI);
-    	
-    	// Simple PROV-N trace, see <http://www.w3.org/TR/prov-n/>
-    	// Here this is done in a naive way by loading a template 
-    	// from src/main/resources and do string-replace to insert
-    	// our URIs.
-    	String template = IOUtils.toString(getClass().getResourceAsStream("/provTemplate.txt"));
-    	String prov = MessageFormat.format(template, 
-    			helloPrefix, appURI, helloEntity, name);
-    	// Note: PROV-N should be be built using say the PROV Toolbox 
-    	// rather than this naive template approach!
-    	return prov;
+            @Context UriInfo ui) throws IOException {
+        // Get our absolute URI
+        // See http://cxf.apache.org/docs/jax-rs-basics.html#JAX-RSBasics-URIcalculationusingUriInfoandUriBuilder        
+        UriBuilder appUri = ui.getBaseUriBuilder();
+        // Absolute URIs for resources we are to give provenance about
+        URI helloURI = appUri.path(getClass(), "hello").build(name);        
+        
+        // Prepare prefixes for PROV-N qualified names
+        URI appURI = appUri.build("").resolve("../");        
+        URI helloPrefix = helloURI.resolve("./");
+        
+        // The PROV-N qualified name for our /hello/{name} resource
+        String helloEntity = "hello:" + helloPrefix.relativize(helloURI);
+        
+        // Simple PROV-N trace, see <http://www.w3.org/TR/prov-n/>
+        // Here this is done in a naive way by loading a template 
+        // from src/main/resources and do string-replace to insert
+        // our URIs.
+        String template = IOUtils.toString(getClass().getResourceAsStream("/provTemplate.txt"));
+        String prov = MessageFormat.format(template, 
+                helloPrefix, appURI, helloEntity, name);
+        // Note: PROV-N should be be built using say the PROV Toolbox 
+        // rather than this naive template approach!
+        return prov;
     }
 }
 
